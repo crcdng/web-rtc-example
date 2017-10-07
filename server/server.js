e182259f62618a06caf827d810d5207436af48e2
mike <straeubig@i3games.de>
2017-10-07 21:07:50 +0100

    es6ify server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const os = require('os');
-const nodeStatic = require('node-static');
-const https = require('https');
 const fs = require('fs');
+const https = require('https');
+const nodeStatic = require('node-static');
+const os = require('os');
 const socketIO = require('socket.io');
 
 const options = {
@@ -12,40 +12,37 @@ const options = {
 };
 
 const fileServer = new(nodeStatic.Server)('../client');
-const app = https.createServer(options, function(req, res) {
+
+const app = https.createServer(options, (req, res) => {
   fileServer.serve(req, res);
 }).listen(8080);
 
 const io = socketIO.listen(app);
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', (socket) => {
 
-  // convenience function to log server messages on the client
-  function log() {
+  function log(...args) {
     const array = ['Message from server:'];
-    array.push.apply(array, arguments);
+    array.push.apply(array, args);
     socket.emit('log', array);
   }
 
-  socket.on('message', function(message) {
+  socket.on('message', (message) => {
     log('Client said: ', message);
     // for a real app, would be room-only (not broadcast)
     socket.broadcast.emit('message', message);
   });
 
-  socket.on('create or join', function(room) {
-    log('Received request to create or join room ' + room);
-
-    let numClients = io.engine.clientsCount;
-
-    log('Room ' + room + ' now has ' + numClients + ' client(s)');
+  socket.on('create or join', (room) => {
+    log(`Received request to create or join room ${room}`);
+    const numClients = io.engine.clientsCount;
+    log(`Room ${room} now has ${numClients} ${numClients > 1 ? 'clients' : 'client'}`);
 
     if (numClients === 1) {
       socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
+      log(`Client ID ${socket.id} created room ${room}`);
       socket.emit('created', room, socket.id);
-
     } else if (numClients === 2) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
+      log(`Client ID ${socket.id} created room ${room}`);
       io.sockets.in(room).emit('join', room);
       socket.join(room);
       socket.emit('joined', room, socket.id);
@@ -55,19 +52,20 @@ io.sockets.on('connection', function(socket) {
     }
   });
 
-  socket.on('ipaddr', function() {
-    const ifaces = os.networkInterfaces();
-    for (let dev in ifaces) {
-      ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-          socket.emit('ipaddr', details.address);
-        }
-      });
-    }
-  });
+  // socket.on('ipaddr', function() {
+  //   const ifaces = os.networkInterfaces();
+  //   console.log(ifaces);
+  //   for (let dev in ifaces) {
+  //     ifaces[dev].forEach(function(details) {
+  //       if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+  //         socket.emit('ipaddr', details.address);
+  //       }
+  //     });
+  //   }
+  // });
 
-  socket.on('bye', function(){
-    console.log('received bye');
-  });
+  // socket.on('bye', function(){
+  //   console.log('received bye');
+  // });
 
 });
